2d60cb82f2b8bed41dbf10ab8e766e26179f2a8c
root <root@ip-172-31-1-20.us-west-1.compute.internal>
2019-02-28 05:40:26 +0000

    Add existing file

--- gitpush.sh
+++ gitpush.sh
@@ -23,9 +23,11 @@
 #$ git push origin master
 # Pushes the changes in your local repository up to the remote repository you specified
 
+$USER = 'anchaubey'
+$PASS = 'Mummayou@345'
 git add . 
 git commit -m "Add existing file"
 git push origin master <<EOF
-anchaubey
-Mummayou@345
+$USER
+$PASS
 EOF 
