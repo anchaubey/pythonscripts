374cd2c5431186217c326889034b1df46a5daeae
root <root@ip-172-31-1-20.us-west-1.compute.internal>
2019-03-03 08:58:58 +0000

    Add existing file

--- sshremoterun.py
+++ sshremoterun.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
 
-#!/bin/python
-import sys
-import os
+import paramiko
 
-data = {"user": "ankit",
-        "host": "172.31.4.101",
-        "password": "redhat",
-        "commands": " " .join(sys.argv[1:])}
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='52.53.152.193', username='ankit', password='redhat')
 
-command = "sshpass -p {password} ssh {user}@{host} {commands}"
-os.system(command.format(**data))
+choice = input("Please enter 1 for cpu usage and 2 for disk usage: ")
+
+if choice == '1':
+    command = 'top -n 1 -b'
+elif choice == '2':
+    command = 'df -h'
+else:
+    print("Please provide a valid input.")
+    sys.exit()
+
+stdin, stdout, stderr = ssh.exec_command(command)
+
+for line in stdout.read().splitlines():
+    print(line.decode('utf8'))
+
+ssh.close()
+
+
