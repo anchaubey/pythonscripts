c270f6e800d40a9a6e816c00824875f1a324095e
anchaubey <35536195+anchaubey@users.noreply.github.com>
2020-08-22 14:29:22 +0530

    Create curl_request.py

---
+++ curl_request.py
@@ -0,0 +1,36 @@
+import socket
+import time
+
+
+ip = "127.0.0.1"
+port = 8000
+retry = 5
+delay = 10
+timeout = 3
+
+def isOpen(ip, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        try:
+                s.connect((ip, int(port)))
+                s.shutdown(socket.SHUT_RDWR)
+                return True
+        except:
+                return False
+        finally:
+                s.close()
+
+def checkHost(ip, port):
+        ipup = False
+        for i in range(retry):
+                if isOpen(ip, port):
+                        ipup = True
+                        break
+                else:
+                        time.sleep(delay)
+        return ipup
+
+if checkHost(ip, port):
+        print(ip + " is UP")
+else:
+        print(ip + "is not UP")
